1bf72c0805acb23b8be1ba10b9ce0c9edbd27372
danieltocado <danieltocado2@gmail.com>
2020-08-15 00:17:17 +0200

    [Frontend] Created file upload container.

---
+++ client/src/containers/fileupload/fileupload.jsx
@@ -0,0 +1,76 @@
+import React, { Fragment, useState } from "react";
+import axios from "axios";
+import Message from "./message.jsx"
+
+import "./fileupload.scss";
+
+
+const FileUpload = () => {
+
+const [file, setFile] = useState('');
+const [filename, setFilename] = useState('Choose File');
+const [uploadedFile, setUploadedFile] = useState({});
+const [message, setMessage] = useState('')
+
+const onChange = e => {
+    setFile(e.target.files[0]);
+    setFilename(e.target.files[0].name);
+};
+
+const onSubmit = async e => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('file', file);
+
+    try {
+       const res = await axios.post('/upload', formData, {
+           headers: {
+               'Content-Type': 'multipart/form-data'
+           },
+
+       });
+       
+       const { file_name, file_path } = res.data;
+
+       setUploadedFile({ file_name, file_path })
+
+       setMessage('File uploaded.');
+
+    } catch (error) {
+        if(error.response.status === 500) {
+            setMessage('There was a problem with the server'); 
+        } else {
+            setMessage(error.response.data.message)
+        }
+    }
+}
+
+  return (
+      <div className="container mt-4">
+      {message ? <Message message={message} /> : null}
+      <form onSubmit={onSubmit}>
+        <div className="custom-file mb-4">
+          <input type="file" name="file" className="custom-file-input" id="customFile" onChange={onChange}/>
+          <label className="custom-file-label" htmlFor="customFile">
+            {filename}
+          </label>
+          Nombre: <input type="text" name="file_name"/>
+          Descripción: <input type="text" name="file_description"/>
+        </div>
+
+        <input type="submit" value="Upload" className="btn btn-primary btn-block mt-4"/>
+      </form>
+
+      { uploadedFile ? <div className="row mt-5">
+        <div className="col-md-6 m-auto">
+            <h3 className="text-center"> { uploadedFile.file_name } </h3>
+            <img style={{ width: '100%' }} src={uploadedFile.file_path} alt=""/>
+        </div>
+      </div> 
+      : null }
+
+    </div>
+  );
+};
+
+export default FileUpload;
