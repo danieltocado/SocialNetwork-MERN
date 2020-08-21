ac4a4bf49caa05227d16b4b83d44522ecfda7363
danieltocado <danieltocado2@gmail.com>
2020-08-21 20:24:45 +0200

    [Frontend] Imported dependencies to App.jsx and changed general colors in App.css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,48 +1,54 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import logo from './logo.svg';
+import { GlobalHistory } from './history';
+
 import './App.css';
 import { getInfo } from "./redux/actions/users";
+import 'antd/dist/antd.css'; 
+
 
 //Components
-import Header from './components/header/header.jsx'
+
 import Footer from './components/footer/footer.jsx'
 
 //Containers
 import Register from './containers/register/register.jsx'
 import Login from './containers/login/login.jsx'
-import Home from './containers/home/home.jsx'
+import Index from './containers/index/index.jsx'
 import HomeRegister from "./components/home-register/home-register.jsx";
-import Index from "./containers/index/index.jsx";
+import Home from "./containers/home/home.jsx";
 import FileUpload from "./containers/fileupload/fileupload.jsx"
+import Sidebar from "./components/sidebar/sidebar";
 
 
-function App() {
+class App extends Component {
 
-  useEffect(() => {
+ /* useEffect(() => {
     const token = localStorage.getItem('authToken');
     if(token){
      getInfo()
      .catch(() => localStorage.removeItem('authToken'))
     }
-  }, [])
+  }, []) */
 
+  render(){
   return (
     
-    <BrowserRouter>
-    
-    <Switch>
-      <Route path="/" exact component={Home} /> 
-      <Route path="/home-register" exact component={HomeRegister} /> 
-      <Route path="/home" exact component={Index} /> 
+    <BrowserRouter >
+    <GlobalHistory />
+    <Switch >
+      <Route path="/" exact component={Index} /> 
+      <Route path="/home" exact component={Home} /> 
       <Route path="/login" exact component={Login} />
       <Route path="/register" exact component={Register} />
       <Route path="/upload" exact component={FileUpload} />
+      
+      <Route path="/sidebar" exact component={Sidebar} />
 
-    </Switch>
+    </Switch >
     
   </BrowserRouter>
-  );
+  )};
 }
 
 export default App;
