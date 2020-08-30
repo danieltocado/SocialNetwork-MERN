6e2d578181fdfa3444a943e22cd821518f10ced2
danieltocado <danieltocado2@gmail.com>
2020-08-30 14:05:03 +0200

    [Frontend] Added icons to sidebar. Deleted categories.

--- client/src/components/sidebar/sidebar.jsx
+++ client/src/components/sidebar/sidebar.jsx
@@ -3,7 +3,10 @@ import { useSelector } from 'react-redux';
 import { NavLink } from "react-router-dom";
 import "./sidebar.scss"
 import SidebarCategory from "./sidebarcategory.jsx";
-import HomeIcon from "@material-ui/icons/Home"
+import PersonOutlineOutlinedIcon from '@material-ui/icons/PersonOutlineOutlined';
+import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
+import AccountBoxOutlinedIcon from '@material-ui/icons/AccountBoxOutlined';
+import PeopleAltOutlinedIcon from '@material-ui/icons/PeopleAltOutlined';
 import { Button } from "@material-ui/core"
 import { logout } from "../../redux/actions/users"
 
@@ -11,28 +14,19 @@ function Sidebar() {
 
   const user = useSelector(state => state.user);
 
+  console.log('Id del usuario logeado ' + user._id)
+
   return (
     <div className="sidebar">
       <h1>SIDEBAR</h1>
 
-      <NavLink exact to="/home"><SidebarCategory active text="Inicio"/></NavLink>
-      <NavLink exact to="/explore"><SidebarCategory text="Explorar"/></NavLink>
-      <NavLink exact to="/notifications"><SidebarCategory text="Notificaciones"/></NavLink>
-      <NavLink exact to="/messages"><SidebarCategory text="Mensajes"/></NavLink>
-      <NavLink exact to="/profile"><SidebarCategory text="Perfil"/></NavLink>
+      <NavLink activeClassName="activeLink" className="category" exact to="/home"><HomeOutlinedIcon/> Inicio</NavLink>
+      <NavLink activeClassName="activeLink" className="category" exact to="/explore"><PeopleAltOutlinedIcon/> Explorar</NavLink>
+      <NavLink activeClassName="activeLink" className="category" exact to="/profile"><AccountBoxOutlinedIcon/>Editar perfil</NavLink>
+      <NavLink activeClassName="activeLink" className="category" exact to={"/" + user.username}><PersonOutlineOutlinedIcon/>{user.username}</NavLink>
       <Button variant="outlined" className="sidebar__tweet" fullWidth onClick={logout}>Logout</Button>
       
-      <div className="btns">
-          {user?._id
-          
-          ?
-            
-                <button className="btn_login">{user.name || user.email}</button>
-              
-          :
-            <p>no se ha encontrado user </p>
-          }
-        </div>
+      
     </div>
   )
 }
--- client/src/components/sidebar/sidebarcategory.jsx
+++ client/src/components/sidebar/sidebarcategory.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import "./sidebar.scss"
 
-function SidebarCategory({ active, text }) {
+function SidebarCategory({ active, text, Icon }) {
     return (
         <div className={`category ${active && 'category--active'}`}>
-            
-            <h2>{text}</h2>
+            {Icon}
+            <h2 activeClassName="activeLink">{text}</h2>
         </div>
     )
 }
