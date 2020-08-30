6c04f1e20543d915cb2fc59a8eee1a580646d605
danieltocado <danieltocado2@gmail.com>
2020-08-30 14:16:55 +0200

    [Frontend] Feed and Profile components updated.

--- client/src/containers/feed/feed.jsx
+++ client/src/containers/feed/feed.jsx
@@ -1,22 +1,25 @@
 import React, { useState, useEffect, useSelector, Component } from "react";
 import Axios from "axios";
+import { connect } from 'react-redux'
+import { create } from "../../redux/actions/posts";
 import moment from "moment";
+import { Avatar, Button } from "@material-ui/core"
 import { BackTop } from 'antd';
 import Posts from "../../components/posts/posts.jsx";
 import Post from "../../components/post/post.jsx";
 import "./feed.scss";
 
 
-export default class Feed extends React.Component {
+class Feed extends React.Component {
   state = {
     posts: [],
-    user: {},
+   
   };
 
   componentDidMount() {
     Axios.get("http://localhost:3000/post/all").then((res) => {
       const posts = res.data;
-      const user = res.data.user;
+      
       this.setState({ posts });
       console.log(posts)
     });
@@ -24,39 +27,84 @@ export default class Feed extends React.Component {
 
   handleUpdate = () => {
     //by calling this method react re-renders the component
-    this.componentDidMount();
+    
   };
 
+  onSubmit = (event) => {
+    event.preventDefault();
+    const post = {
+      text: event.target.text.value,
+      image: event.target.image.value,
+      user: this.props.user._id
+    };
+
+    create(post)
+    
+    
+    //window.location.reload()
+
+    console.log(post);
+    this.componentDidMount();
+
+};
+
   render() {
+
+    const posts = this.state.posts;
+    console.log(posts);
     return (
       <div className="feed">
         {/* Header */}
         <div className="feed_header">
           <h2>Inicio</h2>
         </div>
+
+        <div>
+    </div>
         {/* TweetBox */}
-        <Posts />
+        <div className="post_container">
+            <form onSubmit={this.onSubmit}>
+                <div className="post_form">
+                    <Avatar src={this.props.user.avatar}/>
+                    <input placeholder="Qué está pasando?" type="text" name="text"/>
+                
+                </div>
+
+                <input className="post_image" placeholder="Imagen url" type="text" name="image"/>
+                <Button className="post_button" type="submit" onClick={this.handleUpdate}>Tweet</Button>
+            </form>
+        </div>
 
         {/* Post */}
         <button id="feed" onClick={this.handleUpdate}>Update</button>
-        {this.state.posts.map((post, key) => (
-          
+
+        {posts.map((post) => (
           <Post
-            key={key}
-            name={post.name}
-            username={post.username}
+            key={post._id}
+            name={post.user?.name}
+            surname={post.user?.surname}
+            username={post.user?.username}
             text={post.text}
-            avatar="https://i.ytimg.com/vi/2ulh3M9sQM8/maxresdefault.jpg"
+            avatar={post.user?.avatar}
             image={post.image}
             date={moment(post.date).calendar()}
           />
         ))}
+
+
         
       <a href="#feed">UP</a>
-    
-        
-       
+
       </div>
     );
   }
+
 }
+
+const mapStateToProps = (state) => {
+  return {
+      user: state.user
+  }
+}
+
+export default connect(mapStateToProps)(Feed)
--- client/src/containers/profile/profile.jsx
+++ client/src/containers/profile/profile.jsx
@@ -1,95 +1,177 @@
 import React from "react";
-import { useSelector } from 'react-redux';
+import Axios from "axios";
+import { connect } from 'react-redux'
+import { useSelector } from "react-redux";
 import { update } from "../../redux/actions/users";
 import "./profile.scss";
 
-
 const Profile = () => {
-    
-    const user = useSelector(state => state.user);
-
-    const onSubmit = (event) => {
-        event.preventDefault();
-        const user = {
-          name: event.target.name.value,
-          surname: event.target.surname.value,
-          username: event.target.username.value,
-          email: event.target.email.value,
-          birthdate: event.target.birthdate.value,
-          password: event.target.password.value,
-          password2: event.target.password2.value,
-          gender: event.target.gender.value,
-        };
-    
-        update(user)
-                .then(() => {
-                    setTimeout(() => {
-                        //window.location.reload()
-                    }, 1000);
-                })
-                .catch(error => {
-                    console.log(error)
-                });
-        console.log(user);
-    
-    }
-
-    return (
-        <div className="feed">
-        {/* Header */}
-        <div className="feed_header">
-          <h2>Perfil</h2>
-        </div>
-        {user?._id
-        
-        ?
-          
-        <form onSubmit={onSubmit} className="signup-form">
-     
-        <h2>Actualizacion</h2>
-            <p className="hint-text">Crea tu cuenta. Es gratis y tardas menos de un minuto.</p>
-        <div className="form-group">
-                <div className="row">
-                    <div className="col"><input type="text" className="form-control" name="name" placeholder="Nombre" /></div>
-                    <div className="col"><input type="text" className="form-control" name="surname" placeholder="Apellidos" /></div>
-                </div>        	
-        </div>
-        <div className="form-group">
-                <div className="row">
-                    <div className="col"><input type="text" className="form-control" name="username" placeholder="Nombre de usuario" /></div>
-                    <div className="col"><input type="email" className="form-control" name="email" placeholder="Correo electrónico" /></div>
-                </div>        	
-        </div>
-        <div className="form-group">
-                <div className="row">
-                    <div className="col">
-              <select className="form-control" name="gender" required>
-                <option value="value1" selected>Género</option> 
-                <option value="male">Hombre</option>
-                <option value="female">Mujer</option>
-              </select></div>
-                    <div className="col"><input type="date" className="form-control" name="birthdate" placeholder="Fecha de nacimiento" /></div>
-                </div>        	
-        </div>
-        <div className="row">
-                <div className="col"><input type="password" id="password" name="password" className="form-control" name="first_name" placeholder="Introduce tu contraseña" /></div>
-                <div className="col"><input type="password" id="password2" name="password2" className="form-control" name="last_name" placeholder="Repite tu contraseña" /></div>
-          
-            </div> 
-        
-            <div className="form-group">
-          <button type="submit" className="btn btn-success btn-lg btn-block">Actualisa</button>
-        </div>
-         
-                         
-        </form>
-            
-        :
-          <p>no se ha encontrado user </p>
-        }
-      </div>
-    );
   
-}
+  const user = useSelector((state) => state.user);
 
-export default Profile;
+  const onSubmit = (event) => {
+    console.log(user._id)
+
+    event.preventDefault();
+    const userupdate = {
+      name: event.target.name.value,
+      surname: event.target.surname.value,
+      username: event.target.username.value,
+      //email: event.target.email.value,
+      birthdate: event.target.birthdate.value,
+      avatar: event.target.avatar.value,
+      bio: event.target.bio.value,
+      gender: event.target.gender.value,
+      
+    };
+  
+    update(user._id, userupdate)
+    
+    console.log("Usuario actualizado ", user._id)
+  };
+  
+
+  return (
+    <div className="feed">
+      {/* Header */}
+      <div className="feed_header">
+        <h2>Actualización de perfil</h2>
+      </div>
+      <div className="post_container">
+      <div className="user_container">
+          <div className="user_info">
+            <div className="user_left">
+              <img src={user.avatar} className="profile_avatar" alt="" srcset=""/>
+            </div>
+
+            <div className="user_right">
+              <h1>{user.name} {user.surname}</h1> 
+              <span className="username">@{user.username}</span>
+
+              <h3>{user.email}</h3>
+              <h4>{user.bio}</h4>
+
+              <h3>Following: 
+              Followers:
+            </h3>
+
+            </div>
+          </div>
+          
+
+          <div className="user_bottom">
+           
+          
+          {/*<Link to={"/" + userProfile?.user.username + "/followers"}><h3>Followers</h3></Link> */}
+          </div>
+        </div>
+        </div>
+      {user?._id ? (
+        
+        <form onSubmit={onSubmit} className="signup-form">
+          
+          <h2>Actualización</h2>
+          <p className="hint-text">
+            Actualiza tu cuenta. Añade una foto de perfil y una pequeña biografía tuya!
+          </p>
+          <div className="form-group">
+            <div className="row">
+              <div className="col">
+                <input
+                  type="text"
+                  className="form-control"
+                  name="name"
+                  placeholder={user.name}
+                />
+              </div>
+              <div className="col">
+                <input
+                  type="text"
+                  className="form-control"
+                  name="surname"
+                  placeholder={user.surname}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="row">
+              <div className="col">
+                <input
+                  type="text"
+                  className="form-control"
+                  name="username"
+                  placeholder={user.username}
+                />
+              </div>
+              <div className="col">
+                <input
+                  type="email"
+                  className="form-control"
+                  name="email"
+                  placeholder={user.email}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="row">
+              <div className="col">
+                <select className="form-control" name="gender"  defaultValue="value1" required>
+                  <option value="value1">
+                    Género
+                  </option>
+                  <option value="male">Hombre</option>
+                  <option value="female">Mujer</option>
+                </select>
+              </div>
+              <div className="col">
+                <input
+                  type="date"
+                  className="form-control"
+                  name="birthdate"
+                  placeholder="Fecha de nacimiento"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="form-group">
+              <div className="col">
+                <input
+                  type="textarea"
+                  cols="40" rows="5"
+                  className="form-control"
+                  name="bio"
+                  placeholder="Escribe unas pocas líneas sobre tí."
+                />
+              </div>
+           </div>
+           <div className="form-group">
+              <div className="col">
+                <input
+                  type="textarea"
+                  cols="40" rows="5"
+                  className="form-control"
+                  name="avatar"
+                  placeholder="Enlace de tu avatar."
+                />
+              </div>
+              
+          </div>
+          
+
+          <div className="form-group">
+            <button type="submit" className="btn btn-success btn-lg btn-block">
+              Actualiza
+            </button>
+          </div>
+        </form>
+      ) : (
+        <p>no se ha encontrado user </p>
+      )}
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({user: state.user});
+export default connect(mapStateToProps)(Profile);
