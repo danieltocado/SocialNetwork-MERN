dcf79b8d020f69c5fbbb42e99b6e7200f73bd2ba
danieltocado <danieltocado2@gmail.com>
2020-09-01 21:41:23 +0200

    [Backend] Added new routes to follows.

--- server/routes/follows.js
+++ server/routes/follows.js
@@ -4,11 +4,13 @@ const auth = require("../middlewares/auth.js");
 
 const FollowController = require("../controllers/FollowController");
 
+
 const router = express.Router();
 
 router.post("/follow", auth, FollowController.startFollow)
 router.delete("/follow/:id", auth, FollowController.stopFollow);
 router.get("/following/:id", auth, FollowController.followingUsers);
+router.get("/following/posts/:id", auth, FollowController.followingUserPosts); 
 router.get("/followed/:id?", auth, FollowController.followedUsers);
 router.get("/follows/:followed?", auth, FollowController.userFollows);
 
